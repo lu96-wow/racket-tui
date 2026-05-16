22ae29b71077403dbf69f8f633548073ff838fae
lu96wow <lookshadow123@outlook.com>
2026-05-16 21:38:24 +0800

    add main.rkt

--- main.rkt
+++ main.rkt
@@ -7,7 +7,8 @@
          "output.rkt"
          "output-color.rkt"
          "ansi-format.rkt"
-         "screen-buffer.rkt")
+         "screen-buffer.rkt"
+         "build-input.rkt")
 
 (provide (all-from-out "tui.rkt")
          (all-from-out "resize.rkt")
@@ -16,4 +17,5 @@
          (all-from-out "output.rkt")
          (all-from-out "output-color.rkt")
          (all-from-out "ansi-format.rkt")
-         (all-from-out "screen-buffer.rkt"))
+         (all-from-out "screen-buffer.rkt")
+         (all-from-out "build-input.rkt"))
