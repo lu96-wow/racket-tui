46d4f88042ce164a646ead4829cc71766bf8fba4
lu96wow <lookshadow123@outlook.com>
2026-05-16 19:30:13 +0800

    fix: matrix-buffer local get-style-bytes also needs current-screen #f to capture colors

--- test/matrix-buffer.rkt
+++ test/matrix-buffer.rkt
@@ -12,7 +12,8 @@
 (define (get-style-bytes style-name)
   (call-with-output-bytes
    (λ (out)
-     (parameterize ([current-output-port out])
+     (parameterize ([current-output-port out]
+                    [current-screen #f])
        (style-apply! style-name)))))
 
 (with-tui
