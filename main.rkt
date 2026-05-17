907369b70bb2d453b2803079266e66c4f75addee
lu96wow <lookshadow123@outlook.com>
2026-05-17 10:49:28 +0800

    Revert "add main.rkt"
    This reverts commit 22ae29b71077403dbf69f8f633548073ff838fae.

--- main.rkt
+++ main.rkt
@@ -7,8 +7,7 @@
          "output.rkt"
          "output-color.rkt"
          "ansi-format.rkt"
-         "screen-buffer.rkt"
-         "build-input.rkt")
+         "screen-buffer.rkt")
 
 (provide (all-from-out "tui.rkt")
          (all-from-out "resize.rkt")
@@ -17,5 +16,4 @@
          (all-from-out "output.rkt")
          (all-from-out "output-color.rkt")
          (all-from-out "ansi-format.rkt")
-         (all-from-out "screen-buffer.rkt")
-         (all-from-out "build-input.rkt"))
+         (all-from-out "screen-buffer.rkt"))
