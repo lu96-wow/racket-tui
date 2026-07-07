7e44fc7760c6682db6ee63087b895afa3c22c28c
lu96wow <lookshadow123@outlook.com>
2026-07-07 22:29:29 +0800

    fix info.rkt

--- info.rkt
+++ info.rkt
@@ -4,7 +4,7 @@
 (define version "0.0.1")
 (define description "Terminal UI library for Racket")
 (define authors '("lu96-wow"))
-(define license "MIT")
+(define license '(MIT))
 
 (define collection "tui")
 (define depends '("base"))
