182d7c3f848273bd6f860f31428d0d7c013d1289
lu96 <you@wow.com>
2026-09-17 13:13:00 +0800

    ok

--- info.rkt
+++ info.rkt
@@ -1,11 +1,11 @@
 #lang info
 
-(define name "tui")
-(define version "0.0.1")
-(define description "Terminal UI library for Racket (Linux only)")
-(define authors '("lu96-wow"))
-(define license '(MIT))
-
 (define collection "tui")
-(define build-deps '("racket-lib"))
+(define version "0.0.1")
+(define deps '("racket-lib"))
+(define build-deps '("scribble-lib" "racket-doc"))
+(define scribblings '(("index.scrbl" ())))
+(define pkg-desc "Terminal UI library for Racket (Linux only)")
+(define pkg-authors '("lu96-wow"))
+(define license 'MIT)
 (define compile-omit-paths '("demo"))
